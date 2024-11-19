7b627a708ed35e26aa5583f0fffb5f24cd8f7b58
LMckenna <lmckenna090@gmail.com>
2024-11-18 22:01:04 -0500

    Player spawns from metad

--- src/main/java/TBZ/world/Level.java
+++ src/main/java/TBZ/world/Level.java
@@ -6,7 +6,11 @@ import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
-import org.json.*;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 // local imports
 import TBZ.world.entity.Position;
 
@@ -14,16 +18,17 @@ public class Level {
     public final List<String> level;
     public final Position bounds;
     // TODO - add all fields that correspond to metadat
-    public final Position playerSpawn;
+    private Position playerSpawn;
     private Position[] enemySpawns;
     // public final Door[] doors;
 
-    public Level(String levelPath, String metaPath) {
+    public Level(String levelPath, String metaPath, int levelIndex) {
         this.level = Level.loadLevel(levelPath);
         this.bounds = new Position(this.level.get(0).length(), this.level.size());
-        this.playerSpawn = new Position(3, 2);
-        this.parseMetadata(metaPath);
+        this.parseMetadata(metaPath, levelIndex);
     }
+
+    // getters/setters
 
     public List<String> getLevel() {
         return this.level;
@@ -31,6 +36,14 @@ public class Level {
 
     public Position getPlayerSpawn() {
         return this.playerSpawn;
+    }
+
+    private void setPlayerSpawn(Position position) {
+        this.playerSpawn = position;
+    }
+
+    public void setEnemySpawns(Position[] positions) {
+        this.enemySpawns = positions;
     }
 
     /**
@@ -70,8 +83,32 @@ public class Level {
     }
 
     // TODO - We also need some metadata for spawns, etc.
-    private void parseMetadata(String metaPath) {
+    private void parseMetadata(String metaPath, int levelIndex) {
+        try {
+            File reader = new File(metaPath);
+            String content = new String(Files.readAllBytes(Paths.get(reader.toURI())));
+            
+            JSONObject obj = new JSONObject(content);
+            JSONObject levelObj = obj.getJSONObject(Integer.toString(levelIndex));
 
+            // set player spawn
+            JSONObject playerSpawnObj = levelObj.getJSONObject("playerSpawn");
+            Position playerSpawn = new Position(playerSpawnObj.getInt("x"), playerSpawnObj.getInt("y"));
+            this.setPlayerSpawn(playerSpawn);
+
+            // maybe? : 
+            //// check if level is valid -  
+            //// if any of the lengths are not equal, it's invalid
+            //int tmpLength = level[1].length();
+            //for (int i = 1; i < level.length; i++) {
+            //    if (tmpLength != level[i].length()) {
+            //        String[] errorReport = {"Error - Invalid level format"};
+            //        return errorReport;
+            //    } else tmpLength = level[i].length();
+            //}
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-
 }
